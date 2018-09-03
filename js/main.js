c194faa800981d2c7b2cc7fc617b05c364e72f71
RZD-CN-CD-003\Zhuni <zhuni.yang@razerzone.com>
2018-09-03 18:02:24 +0800

    modify3

--- js/main.js
+++ js/main.js
@@ -1,29 +1,54 @@
 //将代码写进#code和styleTag里面
-function writeCode(code,fn) {
+function writeCss(prefix, code, fn) {
 	let domCode = document.querySelector('#code')
 	let n = 0
 	let id = setInterval(() => {
 		n += 1
-		domCode.innerHTML = Prism.highlight(code.substring(0, n), Prism.languages.css)
-		styleTag.innerHTML = code.substring(0, n)
-		if (n >= result.length) {
+		domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
+		styleTag.innerHTML = prefix + code.substring(0, n)
+		//能拉的最大高度
+		domCode.scrollTop=domCode.scrollHeight
+		if(n >= code.length) {
+			window.clearInterval(id)
+			fn.call()
+		}
+	}, 5)
+
+}
+
+function writeMarkDown(markdown,fn){
+	let domPaper = document.querySelector('#paper>.content')
+	let n=0
+	let id = setInterval(() => {
+		n += 1
+		domPaper.innerHTML = markdown.substring(0, n)
+		domPaper.scrollTop=domPaper.scrollHeight		//能拉的最大高度
+		if(n >= markdown.length) {
 			window.clearInterval(id)
 			fn.call()
 		}
 	}, 10)
-
+	
 }
 
+var css1 = `
 
-var result = `
+/*
+ * 你好
+ * 只用文字做自我介绍太简单
+ * 我就用代码做一个介绍吧
+ * 首先,准备一些样式
+ */
 *{
 	transition:all 1s;
 }
+
 /* 变 个 背 景 吧*/
 html{
 	background:rgb(222,222,222);
 	font-size:16px;
 }
+
 /* 来 个 边 框 */
 #code{
 	border:1px solid red;
@@ -46,42 +71,102 @@ html{
 	color: #DD4A68;
 }
 
+/* 加 一 个 呼 吸 效 果 */
+#code{
+  animation: breath 0.5s infinite alternate-reverse;
+}
+
 /* 加 点 3D 效 果 */
 #code{
 	transform:rotate(360deg)
 }
 
 /* 不 玩 了, 介 绍 开 始  */
-/* 我 需 要 一 张 白 纸  */
+
+#code{
+	position:fixed;
+	left:0;
+	width:50%;
+	height:100%;
+}
+#paper{
+	position:fixed;
+	right:0;
+	width:45%;
+	height:100%;
+	background:grey;
+	padding:16px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+}
+#paper > .content{
+	background:white;
+	height:100%;
+	width:100%;
+}
+
+/* 请 看 右 边 */
+
 `
 
-writeCode(result, () => {
-	console.log('xxx')
-	createPaper()
-}
-)
+var css2 = `
+/* 接下来用一个优秀的库 marked.js
+ * 把 Markdown 变成 HTML
+ */
+`
 
-function createPaper() {
+var md =`
+#自我介绍
+我叫xxx
+1990年4月15日出生
+xxx学校毕业
+
+#技能介绍
+
+#项目介绍
+1.xxx
+2.xxx
+3.xxx
+
+#联系方式
+QQ:
+Email:
+手机:
+`
+var css3 = `
+/* 这就是我的自我介绍
+ * 谢谢观看
+ */
+`
+writeCss('', css1, () => {
+	createPaper(() => {
+		writeMarkDown(md,()=>{
+			writeCss(css1,css2,()=>{
+				convertMarkdownToHtml(()=>{
+					css1+css2,css3,()=>{
+						console.log('finished')
+					}
+				})
+			})
+		})
+	})
+})
+
+function createPaper(fn) {
 	var paper = document.createElement('div')
 	paper.id = 'paper'
 	document.body.appendChild(paper)
+	var content=document.createElement('pre')
+	content.className='content'
+	paper.appendChild(content)	
+	fn.call()
 }
-
-function fn3() {
-	var result1 = `
-#paper{
-	width:100px;
-	height:100px;
-	background:red;
-}`
-	var n = 0
-	var id = setInterval(() => {
-		n = n + 1
-		code.innerHTML = result + result1.substring(0, n)
-		code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css);
-		styleTag.innerHTML = result + result1.substring(0, n)
-		if (n >= result1.length) {
-			window.clearInterval(id)
-		}
-	}, 30)
+function convertMarkdownToHtml(fn){
+  var div = document.createElement('div')  
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn.call()
 }
